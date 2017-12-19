55702c8954bd299fbaa9481bb014ff00c376afad
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2017-12-19 21:20:24 +0900

    時間単位リテラル演算子機能テスト用プロジェクトを更新
    秒のリテラル演算子テストコードを追加

--- cpp14/Chrono Literals/main.cpp
+++ cpp14/Chrono Literals/main.cpp
@@ -11,6 +11,8 @@ int main()
     auto microseconds_f = 3.1us;    // •‚“®¬”“_Œ^‚Ì3.1ƒ}ƒCƒNƒ•b
     auto milliseconds_i = 3ms;      // ®”Œ^‚Ì3ƒ~ƒŠ•b
     auto milliseconds_f = 3.1ms;    // •‚“®¬”“_Œ^‚Ì3.1ƒ~ƒŠ•b
+    auto seconds_i      = 3s;       // ®”Œ^‚Ì3•b
+    auto seconds_f      = 3.1s;     // •‚“®¬”“_Œ^‚Ì3.1•b
 
     std::cout << nanoseconds_i.count() << std::endl;
     std::cout << nanoseconds_f.count() << std::endl;
@@ -18,6 +20,8 @@ int main()
     std::cout << microseconds_f.count() << std::endl;
     std::cout << milliseconds_i.count() << std::endl;
     std::cout << milliseconds_f.count() << std::endl;
+    std::cout << seconds_i.count() << std::endl;
+    std::cout << seconds_f.count() << std::endl;
 
     return 0;
 }
