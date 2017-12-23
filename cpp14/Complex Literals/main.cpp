b55f6baf0351bab4f63e5713b3eed57f833fdd11
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2017-12-23 23:14:53 +0900

    複素数リテラル演算子機能テスト用プロジェクトを更新
    float型のリテラル演算子機能テストコードを追加

--- cpp14/Complex Literals/main.cpp
+++ cpp14/Complex Literals/main.cpp
@@ -7,8 +7,11 @@ int main()
 
     // 実部0.0、虚部1.0の複素数オブジェクトを作る
     std::complex<double> c = 1.0i;
-
     std::cout << c << std::endl;
+
+    // 実部0.0f、虚部1.0fの複素数オブジェクトを作る
+    std::complex<float> cif = 1.0if;
+    std::cout << cif << std::endl;
 
     return 0;
 }
